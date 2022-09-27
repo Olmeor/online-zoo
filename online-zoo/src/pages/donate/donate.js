6117860abd38f588ac31ebbaf087f5fece6c261a
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-09-27 22:28:40 +0700

    refactor: divine css

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -1,3 +1,6 @@
 import '../../assets/styles/fonts.css'
 import '../../assets/styles/normalize.css'
+import './donate_header.css'
+import './donate_donate.css'
+import './donate_footer.css'
 import './donate.css'
