5ed8d84bd6668b10ce30720b682de57614a7970c
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-10-08 22:55:50 +0700

    feat: add donate amount

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -6,8 +6,41 @@ import './donate_media.css'
 import './donate_footer.css'
 import '../../assets/styles/burger.css'
 
-import { openBurger, closeBurger } from '../../assets/js/burger'
+import { burgerOpen, openBurger, closeBurger } from '../../assets/js/burger'
 
-const burgerOpen = document.querySelector('.header__burger_open');
 burgerOpen.onclick = openBurger;
 document.onclick = closeBurger;
+
+const amountArr = document.querySelectorAll('.donate__radio');
+const amountField = document.querySelector('.donate__input');
+
+const initDonate = () => {
+  amountArr[5].checked = true;
+  amountField.value = "100"
+}
+
+initDonate();
+
+const addAmount = (e) => {
+  for (let item of amountArr) {
+    if (item !== e.target) {
+      item.checked = false;
+    }
+  }
+  e.target.checked = true;
+  amountField.value = e.target.value;
+}
+
+amountArr.forEach(e => e.onclick = addAmount);
+
+const recheckAmount = () => {
+  for (let item of amountArr) {
+    if (item.value == amountField.value) {
+      item.checked = true;
+    } else {
+      item.checked = false;
+    }
+  }
+}
+
+amountField.oninput = recheckAmount;
